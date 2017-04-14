c4be52fb007e4aab5147a003091820e335bac1b7
Srini <srinivaspolishetty@gmail.com>
2017-04-14 21:16:29 +0530

    added the images and files
    created the pages and the folders

---
+++ js/modules/main/main.js
@@ -0,0 +1,13 @@
+;(function(angular){
+	"use strict"
+	angular.module("mainapp",['ngStorage', 'ui.router'])
+	.controller("maincontroller", function($scope){
+		$scope.onload = function()
+		{
+			
+		}
+
+
+	});
+})(angular);
+
